ca7e3d5b0631b70d903d278f99235e3d3dc236f7
Laptop Crepes tecl90 <cristianzapata17@gmail.com>
2025-12-09 14:40:36 -0500

    step5Benefit styles upgrade css

--- src/components/steps/Step5Benefits.jsx
+++ src/components/steps/Step5Benefits.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Step5Benefits.css'
 
+import Caja1 from '/src/assets/Cajas/Caja 1.png'
+
 import mejoraViviendaTool from '/src/assets/Iconos/Icono 4.png'
 import mejoraViviendaFolio from '/src/assets/Iconos/Icono 5.png'
 import mejoraViviendaMoney from '/src/assets/Iconos/Icono 6.png'
+
 import personaje from '/src/assets/Personajes/Personaje 4.png'
 
 export default function Step5Benefits({ next, prev }) {
@@ -11,65 +14,60 @@ export default function Step5Benefits({ next, prev }) {
     <div className="benefits-container">
 
       <h3 className="benefits-title">
-        ¡Como ya cuentas con vivienda propia,
-        <br />
+        ¡Como ya cuentas con vivienda propia,<br />
         tenemos estos beneficios para ti!
       </h3>
 
-      {/* Contenedor tarjetas + personaje */}
       <div className="benefits-content">
 
-        {/* GRID DE TARJETAS */}
+        {/* GRID */}
         <div className="benefits-grid">
 
           {/* Tarjeta 1 */}
-          <div className="benefit-card">
-            <h4 className="benefit-card-title">Mejora de vivienda</h4>
+          <div className="benefit-box">
+            <img src={Caja1} className="benefit-box-bg" />
 
-            <div className="benefit-card-content">
-              <p className="benefit-card-text">
-                Conoce y solicita los subsidios
-                <br />
-                para mejorar tu vivienda.
-              </p>
+            <h4 className="benefit-box-title">Mejora de <br />vivienda</h4>
 
-              <img src={mejoraViviendaTool} className="benefit-card-image" />
-            </div>
+            <p className="benefit-box-text">
+              Conoce y solicita los subsidios <br />
+              para mejorar tu vivienda.
+            </p>
+
+            <img src={mejoraViviendaTool} className="benefit-box-icon" />
           </div>
 
           {/* Tarjeta 2 */}
-          <div className="benefit-card">
-            <h4 className="benefit-card-title">Legalización de Documentos</h4>
+          <div className="benefit-box">
+            <img src={Caja1} className="benefit-box-bg" />
 
-            <div className="benefit-card-content">
-              <p className="benefit-card-text">
-                Pon al día tus escrituras, impuestos
-                <br />
-                u otros documentos.
-              </p>
+            <h4 className="benefit-box-title">Legalización de <br />Documentos</h4>
 
-              <img src={mejoraViviendaFolio} className="benefit-card-image" />
-            </div>
+            <p className="benefit-box-text">
+              Pon al día tus escrituras, impuestos <br />
+              u otros documentos.
+            </p>
+
+            <img src={mejoraViviendaFolio} className="benefit-box-icon" />
           </div>
 
           {/* Tarjeta 3 */}
-          <div className="benefit-card">
-            <h4 className="benefit-card-title">Administración de viviendas</h4>
+          <div className="benefit-box">
+            <img src={Caja1} className="benefit-box-bg" />
 
-            <div className="benefit-card-content">
-              <p className="benefit-card-text">
-                Solicita información en caso de herencia
-                <br />
-                o ayuda para sucesión.
-              </p>
+            <h4 className="benefit-box-title">Administración de <br />viviendas</h4>
 
-              <img src={mejoraViviendaMoney} className="benefit-card-image" />
-            </div>
+            <p className="benefit-box-text">
+              Solicita información en caso de herencia <br />
+              o ayuda para sucesión.
+            </p>
+
+            <img src={mejoraViviendaMoney} className="benefit-box-icon" />
           </div>
 
         </div>
 
-        {/* PERSONAJE A LA DERECHA */}
+        {/* Personaje */}
         <div className="benefits-character">
           <img src={personaje} alt="Personaje" />
         </div>
@@ -78,7 +76,6 @@ export default function Step5Benefits({ next, prev }) {
 
       <div className="benefits-controls">
         <button className="benefits-btn-back" onClick={prev}>Volver</button>
-        <button className="benefits-btn-next" onClick={next}>Finalizar</button>
       </div>
 
     </div>
